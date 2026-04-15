1d9fa43718a872939c3b60564fb6f07e24fcd254
victo <victorcordeirofp@gmail.com>
2026-04-15 18:51:39 -0300

    refactor: alterando a extensao gerada pelo arquivo db na conexao

--- estacionamento_db/database/connection.py
+++ estacionamento_db/database/connection.py
@@ -4,5 +4,5 @@ import time
 
 def connect_db():
     time.sleep(3)  # Simulate connection delay
-    sqlite3.connect("database.db")
+    sqlite3.connect("database.sqlite")
     return print("Conexão estabelecida!")
