fc060712c178de15371903315026baaa2828abcc
victo <victorcordeirofp@gmail.com>
2026-04-15 19:40:02 -0300

    fix: problema de identação

--- estacionamento_db/database/connection.py
+++ estacionamento_db/database/connection.py
@@ -2,10 +2,12 @@ import sqlite3
 import os
 from .models import cliente
 
+db_path = os.path.join(os.path.dirname(__file__), "database.sqlite")
+
 
 def connect_db():
-    db_path = os.path.join(os.path.dirname(__file__), "database.sqlite")
     conn = sqlite3.connect(db_path)
     print("Conexão estabelecida!")
     cliente.create_cliente_table(conn)
+    # Cria a tabela cliente se ela não existir
     return conn
