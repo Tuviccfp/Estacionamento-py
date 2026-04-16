5ec0f5ad037ccb21e3cb4f65fa8c80f7c27fefda
victo <victorcordeirofp@gmail.com>
2026-04-15 21:54:10 -0300

    refactor: retirando print que atualmente é desnecessario

--- estacionamento_db/database/connection.py
+++ estacionamento_db/database/connection.py
@@ -7,7 +7,6 @@ db_path = os.path.join(os.path.dirname(__file__), "database.sqlite")
 
 def connect_db():
     conn = sqlite3.connect(db_path)
-    print("Conexão estabelecida!")
     cliente.create_cliente_table(conn)
     # Cria a tabela cliente se ela não existir
     return conn
